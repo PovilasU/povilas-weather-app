661d18b409f592b7812b20039058ffcb37faa4f4
PovilasU <povilas.urbonas85@gmail.com>
2023-12-04 14:53:48 +0000

    optimised code

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,57 +1,39 @@
-import { reformatDate } from './utils'
+import { reformatDate } from "./utils";
 export default ({ info, type }) => {
+  let { temperature_2m_max, temperature_2m_min, temperature_2m, time } =
+    info[type.toLowerCase()];
 
+  if (type === "Hourly") {
+    const currentTime = new Date();
+    const filteredTime = time
+      .filter((_, idx) => idx % 5 !== 0 && new Date(time[idx]) > currentTime)
+      .slice(0, 5);
+    time = filteredTime;
+    temperature_2m = temperature_2m
+      .filter((_, idx) => idx % 5 !== 0 && new Date(time[idx]) > currentTime)
+      .slice(0, 5);
+  }
 
-    let {
-        temperature_2m_max,
-        temperature_2m_min,
-        temperature_2m,
-        time
-    } = info[type.toLowerCase()]
-    /*
-        ==
-        !=
-        ===
-        !==
-    */
-
-    if (type === 'Hourly') {
-        const currentTime = new Date();
-        time = time.filter((x, idx) => {
-            return idx % 5 !== 0 && (new Date(x) > currentTime)
-        }).slice(0, 5)
-        temperature_2m = temperature_2m.filter((x, idx) => {
-            return idx % 5 !== 0 && (new Date(time[idx]) > currentTime)
-        }).slice(0, 5)
-    }
-
-
-    return (
-
-        <>
-            <h2>{type} Forecast</h2>
-            <section className='week-forecast-wrapper module'>
-
-
-                {Object.values(time).map((day, i) => (
-                    <div key={day} className='week-forecast'>
-                        <div>{reformatDate(day)}</div>
-                        {type === 'Daily' ? (
-                            <>
-                                <div>{Object.values(temperature_2m_max)[i]}&#176;C</div>
-                                <div>{Object.values(temperature_2m_min)[i]}&#176;C</div>
-                            </>
-                        ) : (
-                            <>
-                                <div>{Object.values(temperature_2m)[i]}&#176;C</div>
-                            </>
-                        )}
-
-
-                    </div>
-
-                ))}
-            </section>
-        </>
-    )
-}
+  return (
+    <>
+      <h2>{type} Forecast</h2>
+      <section className="week-forecast-wrapper module">
+        {Object.values(time).map((day, i) => (
+          <div key={day} className="week-forecast">
+            <div>{reformatDate(day)}</div>
+            {type === "Daily" ? (
+              <>
+                <div>{Object.values(temperature_2m_max)[i]}&#176;C</div>
+                <div>{Object.values(temperature_2m_min)[i]}&#176;C</div>
+              </>
+            ) : (
+              <>
+                <div>{Object.values(temperature_2m)[i]}&#176;C</div>
+              </>
+            )}
+          </div>
+        ))}
+      </section>
+    </>
+  );
+};
