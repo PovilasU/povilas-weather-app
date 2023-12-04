f3c35b0e5c30b1c14d0856ce2e75863278a674d4
PovilasU <povilas.urbonas85@gmail.com>
2023-12-04 15:00:55 +0000

    ofitised formFields component

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -1,16 +1,15 @@
-export default function FormField({ title, name, placeholder }) {
-    return (
-        <div className="form-field">
-            <label>
-                {title}
-            </label>
-            <input
-                type="text"
-                name={name}
-                autoComplete="off"
-                placeholder={placeholder}
-            />
-        </div>
-    )
-}
+import React from "react";
 
+const FormField = ({ title, name, placeholder }) => (
+  <div className="form-field">
+    <label>{title}</label>
+    <input
+      type="text"
+      name={name}
+      autoComplete="off"
+      placeholder={placeholder}
+    />
+  </div>
+);
+
+export default React.memo(FormField);
