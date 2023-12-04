6e181655e925436641628a146832b805ee26ea08
PovilasU <povilas.urbonas85@gmail.com>
2023-12-04 15:03:46 +0000

    Optimised LocationSearch component

--- src/components/LocationSearch.jsx
+++ src/components/LocationSearch.jsx
@@ -1,36 +1,35 @@
-import { useState } from 'react'
-import Formfield from './FormField';
+import { useState } from "react";
+import Formfield from "./FormField";
+
+const API_ROOT =
+  "https://geocoding-api.open-meteo.com/v1/search?format=json&count=10";
+
 export default function LocationSearch(props) {
-    const API_ROOT = 'https://geocoding-api.open-meteo.com/v1/search?format=json&count=10';
-    let [errorMsg, setErrorMsg] = useState(null)
+  const [errorMsg, setErrorMsg] = useState(null);
 
-    async function handleSearch(e) {
-        e.preventDefault()
-        setErrorMsg(null)
-        let form = e.currentTarget
-        let location = form.location.value
-        let res = await fetch(API_ROOT + `&name=${location}`)
-        let locations = await res.json()
-        if (locations.results) {
-            props.updateLocations(locations.results)
-        } else {
-            setErrorMsg('No results found')
-            props.updateLocations([])
-        }
+  async function handleSearch(e) {
+    e.preventDefault();
+    setErrorMsg(null);
+    const form = e.currentTarget;
+    const location = form.location.value;
+    const res = await fetch(`${API_ROOT}&name=${location}`);
+    const locations = await res.json();
+    if (locations.results) {
+      props.updateLocations(locations.results);
+    } else {
+      setErrorMsg("No results found");
+      props.updateLocations([]);
     }
-    return (
-        <form id="location-search" className='module' onSubmit={handleSearch}>
-            <h2>Enter a location</h2>
-            <Formfield title="Location" name="location" placeholder="Location" />
-            <div className='jcsb'>
-                <div className='error'>
-                    {errorMsg}
-                </div>
-                <button>
-                    Search
-                </button>
-            </div>
-        </form>
-    )
-}
+  }
 
+  return (
+    <form id="location-search" className="module" onSubmit={handleSearch}>
+      <h2>Enter a location</h2>
+      <Formfield title="Location" name="location" placeholder="Location" />
+      <div className="jcsb">
+        <div className="error">{errorMsg}</div>
+        <button>Search</button>
+      </div>
+    </form>
+  );
+}
