97bc2f765e57c0bbbf8340c19042666334f5e18f
PovilasU <povilas.urbonas85@gmail.com>
2023-12-04 15:02:32 +0000

    optimised location results component

--- src/components/LocationResults.jsx
+++ src/components/LocationResults.jsx
@@ -1,25 +1,20 @@
 export default function LocationResults({ locations, updateSelection }) {
-    console.log("locatios")
-    console.log(locations)
-    function handler(location) {
-        updateSelection(location);
-    }
-    return (
-        <div id="location-results" className="module">
-            {locations.map(location => (
-                <div className="location" key={location.id}>
-                    <div className="region">
-                        <div>{location.country} </div>
-                        <div className="subregion">{location.admin1} </div>
-                    </div>
-                    <div>
-                        <button onClick={() => { handler(location) }}>
-                            Select
-                        </button>
-                    </div>
-                </div>
-            ))}
+  function handler(location) {
+    updateSelection(location);
+  }
+  return (
+    <div id="location-results" className="module">
+      {locations.map((location) => (
+        <div className="location" key={location.id}>
+          <div className="region">
+            <div>{location.country} </div>
+            <div className="subregion">{location.admin1} </div>
+          </div>
+          <div>
+            <button onClick={() => handler(location)}>Select</button>
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
-
