dc36b701b815b3e89335ab1ff5672125762b7136
PovilasU <povilas.urbonas85@gmail.com>
2023-12-04 14:41:57 +0000

    Optinising code

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,81 @@
+import { useState, useEffect, useCallback, lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import LocationSearch from './components/LocationSearch';
-import LocationResults from './components/LocationResults';
-import WeatherDisplay from './components/WeatherDisplay';
-import About from './components/About';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-
-/*
-
-*/
+const LocationSearch = lazy(() => import("./components/LocationSearch"));
+const LocationResults = lazy(() => import("./components/LocationResults"));
+const WeatherDisplay = lazy(() => import("./components/WeatherDisplay"));
+const About = lazy(() => import("./components/About"));
 
 function App() {
+  const API_ROOT = `https://api.open-meteo.com/v1/forecast?format=json&daily=temperature_2m_max,temperature_2m_min&hourly=temperature_2m&current_weather=true&`;
+  let [locations, updateLocations] = useState([]);
+  let [selection, updateSelection] = useState(null);
+  let [info, updateInfo] = useState(null);
 
-  /*
-    https://api.music.com/albums?artist=The Beatles&decade=1960s
-      artist: "The Beatles",
-      decade: "1960s"
-
-      wind , humidity
-      images,
-      goto:free incon website
-      
-  */
-
-
-
-  const API_ROOT = `https://api.open-meteo.com/v1/forecast?format=json&daily=temperature_2m_max,temperature_2m_min&hourly=temperature_2m&current_weather=true&`
-  let [locations, updateLocations] = useState([])
-  let [selection, updateSelection] = useState(null)
-  let [info, updateInfo] = useState(null)
-
-  /*
-      {info && (
-        <div>{info.now} {info.temperature} </div>
-      )}
-  */
-
-  async function handleSelectionChange() {
-
+  const fetchData = useCallback(async () => {
     if (selection) {
-      let rest = await fetch(API_ROOT + `latitude=${selection.latitude}&longitude=${selection.longitude}&timezone=${selection.timezone}`);
-      let data = await rest.json();
-      console.log("test")
-      console.log(data);
+      const response = await fetch(
+        `${API_ROOT}latitude=${selection.latitude}&longitude=${selection.longitude}&timezone=${selection.timezone}`
+      );
+      const data = await response.json();
       updateLocations([]);
       updateInfo(data);
     }
-  }
+  }, [selection, API_ROOT]);
 
+  // Call fetchData when selection changes
   useEffect(() => {
-    handleSelectionChange();
-  }, [selection]);
-
-  useEffect(() => {
-    if (locations.length) {
-      updateInfo(null);
-    }
-  }, [locations])
-
+    fetchData();
+  }, [fetchData]);
   return (
     <BrowserRouter>
       <div id="wrapper">
         <header className="module">
           <h1 id="logo">Weather App</h1>
           <nav>
-            <Link to='/'>App</Link>
-            <Link to='/about'>About</Link>
+            <Link to="/">App</Link>
+            <Link to="/about">About</Link>
           </nav>
         </header>
+
         <main>
           <Routes>
-            <Route path='/' element={(<>
-              <LocationSearch locatopns={locations} updateLocations={updateLocations} />
-              {locations.length > 0 &&
-                (<LocationResults
-                  locations={locations}
-                  selection={selection}
-                  updateSelection={updateSelection}
-                />
-                )}
-              {info && <WeatherDisplay info={info} selection={selection} />}
-
-
-            </>)} />
-            <Route path='/about' element={<About />} />
+            <Route
+              path="/"
+              element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <LocationSearch
+                    locations={locations}
+                    updateLocations={updateLocations}
+                  />
+                  {locations.length > 0 && (
+                    <LocationResults
+                      locations={locations}
+                      selection={selection}
+                      updateSelection={updateSelection}
+                    />
+                  )}
+                  {info && <WeatherDisplay info={info} selection={selection} />}
+                </Suspense>
+              }
+            />
+            <Route
+              path="/about"
+              element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <About />
+                </Suspense>
+              }
+            />
           </Routes>
         </main>
         <footer>
           <p>Author: Povilas Urbonas</p>
-          &copy; 2023</footer>
+          &copy; 2023
+        </footer>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
